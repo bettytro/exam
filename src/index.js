3e82ae9205eba7c8d82c44a23eee3e67bec9cab3
bettytro <elisabeth.510@hotmail.com>
2024-05-26 22:00:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import Layout from "./components/layout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <Router>
       <Layout>
         <App />
       </Layout>
     </Router>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
